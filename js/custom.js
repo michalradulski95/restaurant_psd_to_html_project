d9c0107cfd40591f2ce2189b18373def50e52619
michalradulski95 <torcher13@gmail.com>
2019-04-10 23:36:47 +0200

    added mobile version of the site

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 let menus = document.getElementsByClassName('hammburger-menu');
 
 [].forEach.call(menus, function(m) {
     m.addEventListener('click', function() {
         m.parentElement.classList.toggle('open');
     });
-=======
-let menus = document.getElementsByClassName('hammburger-menu');
-
-[].forEach.call(menus, function(m) {
-    m.addEventListener('click', function() {
-        m.parentElement.classList.toggle('open');
-    });
->>>>>>> 2b9ebc9a75701afc658dfb571dd78be30b532525
 });
