c5d819848ff89f31fc2fb84944b82c9542525b9f
michalradulski95 <torcher13@gmail.com>
2019-04-13 13:30:37 +0200

    button active class

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,5 @@
+/*Hamburger menu code*/
+
 const menus = document.getElementsByClassName('hammburger-menu');
 
 [].forEach.call(menus, function(m) {
@@ -5,6 +7,8 @@ const menus = document.getElementsByClassName('hammburger-menu');
         m.parentElement.classList.toggle('open');
     });
 });
+
+/*Banner carousel code*/
 
 const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
@@ -16,20 +20,26 @@ function changebackground() {
     document.getElementById('button1').addEventListener('click', function() {
         document.getElementsByClassName('banner')[0].style.background = "url('images/banner-img.png')";
         document.getElementsByClassName('banner')[0].style.transition = "0.5s ease";
-        document.getElementsByClassName('banner')[0].className += " active";
-
 
     })
     button2.addEventListener('click', function() {
         document.getElementsByClassName('banner')[0].style.background = "url('images/banner-img2.png')";
         document.getElementsByClassName('banner')[0].style.transition = "0.5s ease";
-        document.getElementsByClassName('banner')[0].className += " active";
+
     })
     button3.addEventListener('click', function() {
         document.getElementsByClassName('banner')[0].style.background = "url('images/banner-img3.png')";
         document.getElementsByClassName('banner')[0].style.transition = "0.5s ease";
-        document.getElementsByClassName('banner')[0].className += " active";
+
     })
 }
-
 changebackground();
+let banner = document.getElementById('carousel');
+let btns = banner.getElementsByClassName('button');
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', function() {
+        let current = document.getElementsByClassName('active');
+        current[0].className = current[0].className.replace('active');
+        this.querySelector('div').classList.add('active');
+    })
+};
