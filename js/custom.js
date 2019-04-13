81afa674331366fdc27bae357cc4f63bcfe91d7c
michalradulski95 <torcher13@gmail.com>
2019-04-13 11:15:14 +0200

    changes with small width

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,35 @@
-let menus = document.getElementsByClassName('hammburger-menu');
+const menus = document.getElementsByClassName('hammburger-menu');
 
 [].forEach.call(menus, function(m) {
     m.addEventListener('click', function() {
         m.parentElement.classList.toggle('open');
     });
 });
+
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+const button3 = document.getElementById('button3');
+
+
+
+function changebackground() {
+    document.getElementById('button1').addEventListener('click', function() {
+        document.getElementsByClassName('banner')[0].style.background = "url('images/banner-img.png')";
+        document.getElementsByClassName('banner')[0].style.transition = "0.5s ease";
+        document.getElementsByClassName('banner')[0].className += " active";
+
+
+    })
+    button2.addEventListener('click', function() {
+        document.getElementsByClassName('banner')[0].style.background = "url('images/banner-img2.png')";
+        document.getElementsByClassName('banner')[0].style.transition = "0.5s ease";
+        document.getElementsByClassName('banner')[0].className += " active";
+    })
+    button3.addEventListener('click', function() {
+        document.getElementsByClassName('banner')[0].style.background = "url('images/banner-img3.png')";
+        document.getElementsByClassName('banner')[0].style.transition = "0.5s ease";
+        document.getElementsByClassName('banner')[0].className += " active";
+    })
+}
+
+changebackground();
